bf900c50499a189bc3c80bfb2c2150f972c80821
unknown <miguel.semedo.2@outlook.com>
2022-09-27 23:21:40 +0100

    Get Joke from Joke API

--- script.js
+++ script.js
@@ -105,17 +105,42 @@ const VoiceRSS = {
 	},
 };
 
-function test() {
-	VoiceRSS.speech({
-		key: '4b1d8972c6494a07935a65ee3930a995',
-		src: 'Hello, world!',
-		hl: 'en-us',
-		v: 'Linda',
-		r: 0,
-		c: 'mp3',
-		f: '44khz_16bit_stereo',
-		ssml: false,
-	});
+// function test() {
+// 	VoiceRSS.speech({
+// 		key: '4b1d8972c6494a07935a65ee3930a995',
+// 		src: 'Hello, world!',
+// 		hl: 'en-us',
+// 		v: 'Linda',
+// 		r: 0,
+// 		c: 'mp3',
+// 		f: '44khz_16bit_stereo',
+// 		ssml: false,
+// 	});
+// }
+
+// test();
+
+// Get Jokes from Joke API
+
+async function getJokes() {
+	let joke = '';
+
+	const apiUrl =
+		'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=religious,political';
+
+	try {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+
+		if (data.setup) {
+			joke = `${data.setup} ... ${data.delivery}`;
+		} else {
+			joke = data.joke;
+		}
+		console.log(joke);
+	} catch (err) {
+		console.log('Whoops there is an error: ', err);
+	}
 }
 
-test();
+getJokes();
